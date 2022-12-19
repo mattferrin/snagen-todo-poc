a735d7328c5efaa790d87ad1e216112b44be6293
Matt Ferrin <mattferrin@gmail.com>
2022-12-18 20:30:05 -0800

    proved jest is working, replaced ava

---
+++ source/ui.test.tsx
@@ -0,0 +1,32 @@
+import App from "./ui";
+import ShallowRenderer from "react-test-renderer/shallow";
+import React from "react";
+
+type SnapInput = {
+  stabileSortId: string;
+  comment: string | null;
+  props: Parameters<typeof App>[0];
+};
+
+function snap(input: SnapInput): void {
+  it(input.stabileSortId, () => {
+    /** setup mocks */
+
+    /** unit under test */
+    const renderer = ShallowRenderer.createRenderer();
+    renderer.render(<App {...input.props} />);
+    const result = renderer.getRenderOutput();
+
+    /** assertions */
+    const comment = `${input.comment === "" ? "TODO: comment" : input.comment}`;
+    expect({
+      [comment]: { result },
+    }).toMatchSnapshot();
+  });
+}
+
+snap({
+  stabileSortId: "a6fb76eb-b9bb-4711-bcbe-2168031eecf4",
+  comment: "proved jest is working",
+  props: { name: "Matt" },
+});
